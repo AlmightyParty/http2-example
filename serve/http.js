d6b0fea033c52cd566a5e9b6d9d738809e4e1dc9
yangchongduo <yangchongduo@qudian.com>
2017-06-03 12:18:37 +0800

    upd :xxx

--- serve/http.js
+++ serve/http.js
@@ -6,7 +6,7 @@ var urlObject=url.parse(req.url,true);
 console.log(urlObject);
 var pathname=urlObject.pathname;
 if(pathname=='/go'){
-res.end('http1 server')
+  res.end('http1 server')
 }
 }).listen(9999,()=>{
   console.log('http1 server has created');
